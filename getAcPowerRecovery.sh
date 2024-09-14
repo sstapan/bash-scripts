155e51af29cda7fcab1876dd42207d6360b051e6
Tapan Kesarwani <41117271+sstapan@users.noreply.github.com>
2024-09-14 20:11:24 +0530

    Update getAcPowerRecovery.sh

--- getAcPowerRecovery.sh
+++ getAcPowerRecovery.sh
@@ -2,7 +2,7 @@
 ##################################################################################
 ###### Author: Tapan Kesarwani | version: 1.0.0 | Date Modified: 2021-07-31 ######
 ##################################################################################
-racadm -r $1 -u root -p m4venir2! get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
+racadm -r $1 -u root -p password get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
 echo "$1"
-prec=$(racadm -r $1 -u root -p m4venir2! get bios.SysSecurity.AcPwrRcvry | grep -i AcPwr)
+prec=$(racadm -r $1 -u root -p password get bios.SysSecurity.AcPwrRcvry | grep -i AcPwr)
 echo $prec
