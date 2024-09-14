b2dd9432cf636bef44cec79703dc02d89c457994
Tapan Kesarwani <41117271+sstapan@users.noreply.github.com>
2024-09-14 20:14:17 +0530

    Update getSystemProfile.sh

--- getSystemProfile.sh
+++ getSystemProfile.sh
@@ -2,7 +2,7 @@
 ##################################################################################
 ###### Author: Tapan Kesarwani | version: 1.0.0 | Date Modified: 2021-07-31 ######
 ##################################################################################
-racadm -r $1 -u root -p m4venir2! get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
+racadm -r $1 -u root -p password get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
 echo "$1"
-sprf=$(racadm -r $1 -u root -p m4venir2! get bios.SysProfileSettings.SysProfile | grep -i SysProfile=)
+sprf=$(racadm -r $1 -u root -p password get bios.SysProfileSettings.SysProfile | grep -i SysProfile=)
 echo $sprf
