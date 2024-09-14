ad3c2fe7262b00a4a602eb259ffdf33f8ab9a118
Tapan Kesarwani <41117271+sstapan@users.noreply.github.com>
2024-09-14 20:21:35 +0530

    Update setBiosBootMode.sh

--- setBiosBootMode.sh
+++ setBiosBootMode.sh
@@ -2,10 +2,10 @@
 ##################################################################################
 ###### Author: Tapan Kesarwani | version: 1.0.0 | Date Modified: 2021-07-31 ######
 ##################################################################################
-racadm -r $1 -u root -p m4venir2! get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
+racadm -r $1 -u root -p password get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
 echo "$1"
-bmode=$(racadm -r $1 -u root -p m4venir2! get bios.BiosBootSettings.BootMode | grep -i bootmode)
+bmode=$(racadm -r $1 -u root -p password get bios.BiosBootSettings.BootMode | grep -i bootmode)
 echo $bmode
 echo -e "Changing BootMode for $1 to \"$2\"...\n"
-racadm -r $1 -u root -p m4venir2! set bios.BiosBootSettings.BootMode $2
-racadm -r $1 -u root -p m4venir2! jobqueue create BIOS.Setup.1-1 -r pwrcycle
+racadm -r $1 -u root -p password set bios.BiosBootSettings.BootMode $2
+racadm -r $1 -u root -p password jobqueue create BIOS.Setup.1-1 -r pwrcycle
