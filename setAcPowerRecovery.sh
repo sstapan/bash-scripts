f772345c88527967cb27d38b017c4232c1fb304b
Tapan Kesarwani <41117271+sstapan@users.noreply.github.com>
2024-09-14 20:20:53 +0530

    Update setAcPowerRecovery.sh

--- setAcPowerRecovery.sh
+++ setAcPowerRecovery.sh
@@ -2,10 +2,10 @@
 ##################################################################################
 ###### Author: Tapan Kesarwani | version: 1.0.0 | Date Modified: 2021-07-31 ######
 ##################################################################################
-racadm -r $1 -u root -p m4venir2! get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
+racadm -r $1 -u root -p password get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
 echo "$1"
-prec=$(racadm -r $1 -u root -p m4venir2! get bios.SysSecurity.AcPwrRcvry | grep -i AcPwr)
+prec=$(racadm -r $1 -u root -p password get bios.SysSecurity.AcPwrRcvry | grep -i AcPwr)
 echo $prec
 echo -e "Changing AC Power Recovery for $1 to \"$2\"...\n"
-racadm -r $1 -u root -p m4venir2! set bios.SysSecurity.AcPwrRcvry $2
-racadm -r $1 -u root -p m4venir2! jobqueue create BIOS.Setup.1-1 -r pwrcycle
+racadm -r $1 -u root -p password set bios.SysSecurity.AcPwrRcvry $2
+racadm -r $1 -u root -p password jobqueue create BIOS.Setup.1-1 -r pwrcycle
