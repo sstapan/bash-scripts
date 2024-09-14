a823593f74a214c4e4e1c937b18a9cad9c18bc8e
Tapan Kesarwani <41117271+sstapan@users.noreply.github.com>
2024-09-14 20:11:51 +0530

    Update getBiosBootMode.sh

--- getBiosBootMode.sh
+++ getBiosBootMode.sh
@@ -2,7 +2,7 @@
 ##################################################################################
 ###### Author: Tapan Kesarwani | version: 1.0.0 | Date Modified: 2021-07-31 ######
 ##################################################################################
-racadm -r $1 -u root -p m4venir2! get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
+racadm -r $1 -u root -p password get iDRAC.NIC.DNSRacName | grep DNSRacName | awk -F"=" '{print $2}'
 echo "$1"
-bmode=$(racadm -r $1 -u root -p m4venir2! get bios.BiosBootSettings.BootMode | grep -i bootmode)
+bmode=$(racadm -r $1 -u root -p password get bios.BiosBootSettings.BootMode | grep -i bootmode)
 echo $bmode
